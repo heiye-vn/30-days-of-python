e5cd1b12e0d9058b06efcc44e46445ab3859f797
赵顺萍 <1064239893@qq.com>
2026-06-16 10:24:31 +0800

    modify：修改测试代码，README.md 文件添加提交规范

--- day01/test.py
+++ day01/test.py
@@ -1,4 +1,6 @@
 import sys
 
-print("hello world")
-print(f"🎉 当前 python 版本：{sys.version}")
+print("Hello, World!")
+print(f"系统信息：{sys.version}")
+print(f"当前正在使用的 Python 解释器路径是：{sys.executable}")
+print("🎉 恭喜！你的虚拟环境运行完全正常！")
