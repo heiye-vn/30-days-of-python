d0d2559105aaed526f8cb2199f3cdc578117950e
赵顺萍 <1064239893@qq.com>
2026-06-24 17:41:51 +0800

    modify：添加字符串方法练习

--- day04/exercise01.py
+++ day04/exercise01.py
@@ -7,14 +7,23 @@
 # 2. 提取文件扩展名
 filename2 = 'report.pdf'
 extension = filename2.split('.')[-1]
-print(extension)
+# print(extension)
 
 # 3. 反转字符串
 text = 'python'
-print(text[::-1])
+# print(text[::-1])
 
 # 4. 统计单词出现次数
 # 注：不能直接使用字符串的 count() 方法来统计，因为 count() 方法是统计子字符串出现的次数，而不是单词出现的次数。
 sentence = "apple banana apple orange apple"
 words = sentence.split()
-print(words.count("apple"))
+# print(words.count("apple"))
+
+# 5. 为字符串 'Python For Everyone' 创建首字母缩略词或缩写
+textStr = 'Python For Everyone'
+# 将字符串拆分成单词列表
+wordList = textStr.split()
+# 提取每个单词的首字母并转为大写
+# 将它们拼接在一起
+acronym = "".join([word[0].upper() for word in wordList])
+print(acronym)
